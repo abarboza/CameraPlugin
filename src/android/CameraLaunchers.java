90f774d290cea70ac5e75e84af8459fe041f5c12
Alfredo Barboza <alfie.barboza@gmail.com>
2015-03-03 18:01:34 -0300

    g

--- src/android/CameraLaunchers.java
+++ src/android/CameraLaunchers.java
@@ -1,4 +1,18 @@
+/*
+	   Copyright 2013 Bruno Carreira - Lucas Farias - Rafael Luna - Vin?cius Fonseca.
 
+		Licensed under the Apache License, Version 2.0 (the "License");
+		you may not use this file except in compliance with the License.
+		You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+		Unless required by applicable law or agreed to in writing, software
+		distributed under the License is distributed on an "AS IS" BASIS,
+		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+		See the License for the specific language governing permissions and
+   		limitations under the License.   			
+ */
 
 package com.vmbc.customcamera;
 
@@ -8,11 +22,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.ExifHelper;
+import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -46,11 +56,11 @@ import android.widget.RelativeLayout;
  * redisplayed.
  */
 @SuppressLint("NewApi")
-public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
+public class CameraLauncher extends org.apache.cordova.camera.CameraLauncher {
 
-	private static final String GET_PICTURE = "getPicture";
-	private static final String TAKE_PICTURE = "takePicture";
-	private static final String DESTROY_CAMERA = "destroyCamera";
+	private static final String	GET_PICTURE	= "getPicture";
+	private static final String	TAKE_PICTURE	= "takePicture";
+	private static final String	DESTROY_CAMERA	= "destroyCamera";
 
 	@SuppressWarnings("unused")
 	private int mQuality;
@@ -65,172 +75,166 @@ public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
 	public String callbackId;
 	private int numPics;
 
-	private Activity activity;
-	private ViewGroup root;
+	private Activity	activity;
+	private ViewGroup	root;
 
-	private Camera mCamera;
+	private Camera	mCamera;
 
-	private CameraPreview mPreview;
-	private CallbackContext callbackContext;
+	private CameraPreview	mPreview;
+
+	private static final String _DATA = "_data";
 
 	/**
 	* Constructor.
 	*/
-	public CameraLaunchers() {
+	public CameraLauncher() {
 	}
 
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
-		activity = cordova.getActivity();
-		root = (ViewGroup) webView.getParent();
+	   activity = cordova.getActivity();
+	   root = (ViewGroup) webView.getParent();
 	}
+	
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action        	The action to execute.
+     * @param args          	JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return              	A PluginResult object with a status and message.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    	
+        this.callbackContext = callbackContext;
 
-	/**
-	* Executes the request and returns PluginResult.
-	* 
-	* @param action
-	*            The action to execute.
-	* @param args
-	*            JSONArry of arguments for the plugin.
-	* @param callbackContext
-	*            The callback id used when calling back into JavaScript.
-	* @return A PluginResult object with a status and message.
-	*/
-	public boolean execute(String action, JSONArray args,
-			CallbackContext callbackContext) throws JSONException {
+        if (action.equals(TAKE_PICTURE)) {
+            this.targetHeight = 0;
+            this.targetWidth = 0;
+            this.mQuality = 80;
+            this.targetX = 0;
+            this.targetY = 0;
 
-		this.callbackContext = callbackContext;
+            this.mQuality = ((JSONObject)args.get(0)).optInt("quality");
+            this.targetWidth = ((JSONObject)args.get(0)).optInt("targetWidth");
+            this.targetHeight = ((JSONObject)args.get(0)).optInt("targetHeight");
+            this.targetX = ((JSONObject)args.get(0)).optInt("targetX");
+            this.targetY = ((JSONObject)args.get(0)).optInt("targetY");
 
-		if (action.equals(TAKE_PICTURE)) {
-			this.targetHeight = 0;
-			this.targetWidth = 0;
-			this.mQuality = 80;
-			this.targetX = 0;
-			this.targetY = 0;
-
-			this.mQuality = ((JSONObject) args.get(0)).optInt("quality");
-			this.targetWidth = ((JSONObject) args.get(0)).optInt("targetWidth");
-			this.targetHeight = ((JSONObject) args.get(0))
-					.optInt("targetHeight");
-			this.targetX = ((JSONObject) args.get(0)).optInt("targetX");
-			this.targetY = ((JSONObject) args.get(0)).optInt("targetY");
-
-			// If the user specifies a 0 or smaller width/height
-			// make it -1 so later comparisons succeed
-			if (this.targetWidth < 1) {
-				this.targetWidth = -1;
-			}
-			if (this.targetHeight < 1) {
-				this.targetHeight = -1;
-			}
-			activity.runOnUiThread(new Runnable() {
-
+            // If the user specifies a 0 or smaller width/height
+            // make it -1 so later comparisons succeed
+            if (this.targetWidth < 1) {
+                this.targetWidth = -1;
+            }
+            if (this.targetHeight < 1) {
+                this.targetHeight = -1;
+            }
+            activity.runOnUiThread(new Runnable() {
+				
 				public void run() {
 					takePicture();
 				}
 			});
+  
+            callbackContext.success();
+            return true;
+        } else if (action.equals(GET_PICTURE)) {
+        	// get an image from the camera
+    		mCamera.autoFocus(new AutoFocusCallback() {
 
-			callbackContext.success();
-			return true;
-		} else if (action.equals(GET_PICTURE)) {
-			// get an image from the camera
-			mCamera.autoFocus(new AutoFocusCallback() {
-
-				public void onAutoFocus(boolean success, Camera camera) {
-					mCamera.takePicture(null, null, mPicture);
-				}
-			});
-			return true;
-		} else if (action.equals(DESTROY_CAMERA)) {
-			try {
-				activity.runOnUiThread(new Runnable() {
-
-					public void run() {
-						destryoCamera();
-					}
-				});
-				callbackContext.success();
+    		   public void onAutoFocus(boolean success, Camera camera) {
+    			mCamera.takePicture(null, null, mPicture);
+    		   }
+    		});
+    		return true;
+        } else if (action.equals(DESTROY_CAMERA)) {
+        	try {
+                activity.runOnUiThread(new Runnable() {
+    				
+    				public void run() {
+    					destryoCamera();
+    				}
+    			});
+                callbackContext.success();
 			} catch (Exception e) {
-				callbackContext.error(e.getMessage());
-				;
-			}
-			return true;
+	           callbackContext.error(e.getMessage());;
+			}        	
+        	return true;
 		}
-		return false;
-	}
+        return false;
+    }
 
-	private PictureCallback mPicture = new PictureCallback() {
+    private PictureCallback mPicture = new PictureCallback() {
 
-		@SuppressWarnings("deprecation")
-		public void onPictureTaken(byte[] data, Camera camera) {
+    	public void onPictureTaken(byte[] data, Camera camera) {
 
-			File pictureFile = new File(imageUri.getPath());
 
-			try {
-				FileOutputStream fos = new FileOutputStream(pictureFile);
-				fos.write(data);
-				fos.close();
-			} catch (FileNotFoundException e) {
-				// Log.d(TAG, "File not found: " + e.getMessage());
-			} catch (IOException e) {
-				// Log.d(TAG, "Error accessing file: " + e.getMessage());
+    	   File pictureFile = new File(imageUri.getPath());
 
-			}
-			try {
-				// Create an ExifHelper to save the exif data that is lost
-				// during compression
-				ExifHelper exif = new ExifHelper();
-				exif.createInFile(getTempDirectoryPath(activity
-						.getApplicationContext()) + "/Pic.jpg");
-				exif.readExifData();
-
-				// Read in bitmap of captured image
-				Bitmap bitmap;
-				try {
-					bitmap = android.provider.MediaStore.Images.Media
-							.getBitmap(activity.getContentResolver(), imageUri);
-				} catch (FileNotFoundException e) {
-					android.content.ContentResolver resolver = activity
-							.getContentResolver();
-					bitmap = android.graphics.BitmapFactory
-							.decodeStream(resolver.openInputStream(imageUri));
-				}
-
-				// bitmap = scaleBitmap(bitmap);
-
+    	   try {
+	   		FileOutputStream fos = new FileOutputStream(pictureFile);
+	   		fos.write(data);
+	   		fos.close();
+    	   } catch (FileNotFoundException e) {
+    	   	//    		Log.d(TAG, "File not found: " + e.getMessage());
+    	   } catch (IOException e) {
+    	   	//    		Log.d(TAG, "Error accessing file: " + e.getMessage());
+    	   	
+    	   }
+    	   try {
+    	   	// Create an ExifHelper to save the exif data that is lost
+    	   	// during compression
+    	   	ExifHelper exif = new ExifHelper();
+    	   	exif.createInFile(getTempDirectoryPath(activity.getApplicationContext())
+    	   	                  + "/Pic.jpg");
+    	   	exif.readExifData();
+    	   	
+    	   	// Read in bitmap of captured image
+    	   	Bitmap bitmap;
+    	   	try {
+    	   		bitmap = android.provider.MediaStore.Images.Media
+    	   				.getBitmap(activity.getContentResolver(), imageUri);
+    	   	} catch (FileNotFoundException e) {
+    	   		android.content.ContentResolver resolver = activity.getContentResolver();
+    	   		bitmap = android.graphics.BitmapFactory
+    	   				.decodeStream(resolver.openInputStream(imageUri));
+    	   	}
+    	   	
+//    	    	bitmap = scaleBitmap(bitmap);
+    	   	
 				int orientation = getCameraPhotoOrientation(activity);
 				bitmap = RotateBitmap(bitmap, orientation);
-
+    	   	
 				int rate = 100;
 				ByteArrayOutputStream byteArrayOutputStream;
 				byte[] byteArray;
 				do {
-					byteArrayOutputStream = new ByteArrayOutputStream();
-					bitmap.compress(Bitmap.CompressFormat.JPEG, rate,
-							byteArrayOutputStream);
-					byteArray = byteArrayOutputStream.toByteArray();
-					rate -= 5;
+					byteArrayOutputStream = new ByteArrayOutputStream();  
+	   	    	bitmap.compress(Bitmap.CompressFormat.JPEG, rate, byteArrayOutputStream);
+	   	    	byteArray = byteArrayOutputStream .toByteArray();
+	   	    	rate -= 5;
 				} while (byteArrayOutputStream.size() > 50000 && rate > 10);
-
-				String encoded = Base64.encodeToString(byteArray,
-						Base64.DEFAULT);
-				Log.i("BASE64-IMG", encoded);
-				System.out.println(encoded);
-				callbackContext.success(encoded);
-
-				bitmap.recycle();
-				bitmap = null;
-				System.gc();
-
-				checkForDuplicateImage();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			mPreview.refreshPreview();
-		}
-	};
-
+    	   	
+    	   	
+    	   	String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+    	   	Log.i("BASE64-IMG", encoded);
+    	   	System.out.println(encoded);
+    	   	callbackContext.success(encoded);
+    	   	
+    	   	bitmap.recycle();
+    	   	bitmap = null;
+    	   	System.gc();
+    	   	
+    	   	checkForDuplicateImage();
+    	   } catch (IOException e) {
+    	   	e.printStackTrace();
+    	   	failPicture("Error capturing image.");
+    	   }
+    	   mPreview.refreshPreview();
+    	}
+    };
+    
 	// --------------------------------------------------------------------------
 	// LOCAL METHODS
 	// --------------------------------------------------------------------------
@@ -246,7 +250,7 @@ public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
 	* img.src="data:image/jpeg;base64,"+result; or to display URI in an img tag
 	* img.src=result;
 	* 
-	*/
+	*/    
 	public void takePicture() {
 		if (mCamera != null) {
 			return;
@@ -257,37 +261,34 @@ public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
 
 		this.photo = createCaptureFile();
 		this.imageUri = Uri.fromFile(photo);
-
+		
 		// window layout
-		RelativeLayout windowLayer = new RelativeLayout(activity);
-		windowLayer.setPadding(0, 0, 0, 0);
-		LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT,
-				LayoutParams.MATCH_PARENT);
-		layoutParams.gravity = Gravity.TOP | Gravity.START;
-		windowLayer.setLayoutParams(layoutParams);
-
-		((ViewGroup) webView.getParent()).removeView(webView);
-		root.removeAllViews();
-		FrameLayout.LayoutParams webParams = new FrameLayout.LayoutParams(
-				webView.getLayoutParams());
-		webView.setLayoutParams(webParams);
-		windowLayer.addView(webView);
-
+	   RelativeLayout windowLayer = new RelativeLayout(activity);
+	   windowLayer.setPadding(0, 0, 0, 0);
+	   LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+	   layoutParams.gravity = Gravity.TOP | Gravity.LEFT;
+	   windowLayer.setLayoutParams(layoutParams);
+	   
+	   ((ViewGroup) webView.getParent()).removeView(webView);
+	   root.removeAllViews();
+	   FrameLayout.LayoutParams webParams = new FrameLayout.LayoutParams(webView.getLayoutParams());
+	   webView.setLayoutParams(webParams);
+	   windowLayer.addView(webView);
+	   
 		mCamera = getCameraInstance();
 
 		if (mPreview == null) {
 			// Create a Preview and set it as the content of activity.
 			mPreview = new CameraPreview(activity, mCamera);
 		} else {
-			((ViewGroup) mPreview.getParent()).removeView(mPreview);
+		   ((ViewGroup) mPreview.getParent()).removeView(mPreview);
 		}
-		RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-				targetWidth, targetHeight);
+		RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(targetWidth, targetHeight);
 		lp.topMargin = targetY;
 		lp.leftMargin = targetX;
 		mPreview.setLayoutParams(lp);
 		windowLayer.addView(mPreview);
-
+		
 		root.addView(windowLayer);
 	}
 
@@ -300,19 +301,18 @@ public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
 
 		this.photo = null;
 		this.imageUri = null;
-
-		((ViewGroup) webView.getParent()).removeView(webView);
-		root.removeAllViews();
-		root.addView(webView);
-
-		mCamera.stopPreview();
-		mCamera.release();
+		
+	   ((ViewGroup) webView.getParent()).removeView(webView);
+	   root.removeAllViews();
+	   root.addView(webView);
+	   
+	   mCamera.stopPreview();
+	   mCamera.release();
 		mCamera = null;
-
+		
 		mPreview = null;
 
 	}
-
 	/**
 	* Create a file in the applications temporary directory based upon the
 	* supplied encoding.
@@ -320,46 +320,43 @@ public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
 	* @return a File object pointing to the temporary picture
 	*/
 	private File createCaptureFile() {
-		File photo = new File(getTempDirectoryPath(this.cordova.getActivity()
-				.getApplicationContext()), "Pic.jpg");
+		File photo = new File(getTempDirectoryPath(this.cordova.getActivity().getApplicationContext()), "Pic.jpg");
 		return photo;
 	}
 
-	private int getCameraPhotoOrientation(Context context) {
-		// Fix orientation issue
+	private int getCameraPhotoOrientation(Context context){
+	 //Fix orientation issue
 		int count = Camera.getNumberOfCameras();
-		int cameraId = count == 1 ? CameraInfo.CAMERA_FACING_BACK
-				: CameraInfo.CAMERA_FACING_FRONT;
+		int cameraId = count == 1 ?  CameraInfo.CAMERA_FACING_BACK : CameraInfo.CAMERA_FACING_FRONT;
 		android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
-		android.hardware.Camera.getCameraInfo(cameraId, info);
-		int rotation = ((Activity) context).getWindowManager()
-				.getDefaultDisplay().getRotation();
-		int degrees = 0;
-		switch (rotation) {
-		case Surface.ROTATION_0:
-			degrees = 0;
-			break;
-		case Surface.ROTATION_90:
-			degrees = 90;
-			break;
-		case Surface.ROTATION_180:
-			degrees = 180;
-			break;
-		case Surface.ROTATION_270:
-			degrees = 270;
-			break;
-		}
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = ((Activity) context).getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+        case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+        case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+        case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+        case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
 
-		int result;
-		if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-			result = (info.orientation + degrees) % 360;
-			result = (360 - result) % 360; // compensate the mirror
-		} else { // back-facing
-			result = (info.orientation - degrees + 360) % 360;
-		}
-		return result;
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                result = (info.orientation + degrees) % 360;
+                result = (360 - result) % 360; // compensate the mirror
+        } else { // back-facing
+                result = (info.orientation - degrees + 360) % 360;
+        }
+	   return result;
 	}
-
+	
 	/**
 	* Scales the bitmap according to the requested size.
 	* 
@@ -411,12 +408,9 @@ public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
 	* @return a cursor
 	*/
 	private Cursor queryImgDB() {
-		return this.cordova
-				.getActivity()
-				.getContentResolver()
-				.query(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-						new String[] { MediaStore.Images.Media._ID }, null,
-						null, null);
+		return this.cordova.getActivity().getContentResolver().query(
+				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+				new String[] { MediaStore.Images.Media._ID }, null, null, null);
 	}
 
 	/**
@@ -438,8 +432,7 @@ public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
 					.getColumnIndex(MediaStore.Images.Media._ID))) - 1;
 			Uri uri = Uri.parse(MediaStore.Images.Media.EXTERNAL_CONTENT_URI
 					+ "/" + id);
-			this.cordova.getActivity().getContentResolver()
-					.delete(uri, null, null);
+			this.cordova.getActivity().getContentResolver().delete(uri, null, null);
 		}
 	}
 
@@ -477,22 +470,21 @@ public class CameraLaunchers extends org.apache.cordova.camera.CameraLauncher {
 		Camera c = null;
 		try {
 			int count = Camera.getNumberOfCameras();
-			int cameraId = count == 1 ? CameraInfo.CAMERA_FACING_BACK
-					: CameraInfo.CAMERA_FACING_FRONT;
+			int cameraId = count == 1 ?  CameraInfo.CAMERA_FACING_BACK : CameraInfo.CAMERA_FACING_FRONT;
 			c = Camera.open(cameraId); // attempt to get a
-										// Camera
-										// instance
+																// Camera
+																// instance
 		} catch (Exception e) {
 			// Camera is not available (in use or does not exist)
 		}
 		return c; // returns null if camera is unavailable
 	}
-
-	public static Bitmap RotateBitmap(Bitmap source, float angle) {
-		Matrix matrix = new Matrix();
-		matrix.postRotate(angle);
-		matrix.preScale(-1, 1);
-		return Bitmap.createBitmap(source, 0, 0, source.getWidth(),
-				source.getHeight(), matrix, true);
+	
+	public static Bitmap RotateBitmap(Bitmap source, float angle)
+	{
+	     Matrix matrix = new Matrix();
+	     matrix.postRotate(angle);
+	     matrix.preScale(-1, 1);
+	     return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
 	}
 }
